5a4b7d4e19d9d90ab8a1b33ad0d8fe45cdff634a
manish <manishrajput6196@gmail.com>
2023-10-27 13:51:10 +0530

    add modal provider

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,20 +1,7 @@
 "use client";
 
-import { Modal } from "@/components/ui/modal";
-
 const SetupPage = () => {
-  return (
-    <div className="p-4">
-      <Modal
-        isOpen={true}
-        onClose={() => {}}
-        title="Test"
-        description="Test description"
-      >
-        Children
-      </Modal>
-    </div>
-  );
+  return <div className="p-4">Root Page</div>;
 };
 
 export default SetupPage;
